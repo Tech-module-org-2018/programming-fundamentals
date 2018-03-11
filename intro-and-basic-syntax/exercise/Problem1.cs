5d9eebf0bcb2afdb8cdd3da34e9f9a7adaaf2d5e
svetlozarbg <37265822+svetlozarbg@users.noreply.github.com>
2018-03-11 19:28:54 +0200

    Added comments

--- intro-and-basic-syntax/exercise/Problem1.cs
+++ intro-and-basic-syntax/exercise/Problem1.cs
@@ -10,11 +10,19 @@ namespace ex1
     {
         static void Main(string[] args)
         {
+           // We receive first integer from the console
             int firstNum = int.Parse(Console.ReadLine());
+         
+          // We receive second integer from the console
             int secondNum = int.Parse(Console.ReadLine());
+         
+          // We receive third integer from the console
             int thirdNum = int.Parse(Console.ReadLine());
+         
+          // We receive fourth integer from the console
             int fourthNum = int.Parse(Console.ReadLine());
- 
+         
+          // We print debit card number in 4-digit format
             Console.WriteLine($"{firstNum:d4} {secondNum:d4} {thirdNum:d4} {fourthNum:d4}");
  
         }

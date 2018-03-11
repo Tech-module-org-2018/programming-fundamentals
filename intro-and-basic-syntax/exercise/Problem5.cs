6c023e3559417eb47c11ed9fdde0b61df517d06b
Ruslan <star93@list.ru>
2018-03-11 19:17:40 +0200

    Added comment for problem5

--- intro-and-basic-syntax/exercise/Problem5.cs
+++ intro-and-basic-syntax/exercise/Problem5.cs
@@ -10,14 +10,18 @@ namespace ex5
     {
         static void Main(string[] args)
         {
+	    //Defining name of the input params
             string name = Console.ReadLine();
             int health = int.Parse(Console.ReadLine());
             int healthMax = int.Parse(Console.ReadLine());
             int energy = int.Parse(Console.ReadLine());
             int energyMax = int.Parse(Console.ReadLine());
+
+            //Calculating health and energy
             int healthLeft = healthMax - health;
             int energyLeft = energyMax - energy;
-
+            
+            //Getting the final outputs
             Console.WriteLine($"Name: {name}");
 
             Console.Write($"Health: ");

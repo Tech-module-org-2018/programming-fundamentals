ea69a088da68377830f6c944bcb43bd3ae633d4a
Ruslan <star93@list.ru>
2018-03-11 18:43:39 +0200

    Added problem 1

---
+++ intro-and-basic-syntax/exercise/Problem1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+ 
+            Console.WriteLine($"{firstNum:d4} {secondNum:d4} {thirdNum:d4} {fourthNum:d4}");
+ 
+        }
+    }
+}

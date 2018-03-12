28cee1836ce7f1c6d761f4d5adb4d1dcda8642f4
Ruslan <star93@list.ru>
2018-03-12 18:39:26 +0200

    Changed variables names

--- intro-and-basic-syntax/exercise/Problem3.cs
+++ intro-and-basic-syntax/exercise/Problem3.cs
@@ -4,20 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ex3
+namespace Problem3
 {
-    class Program
+    class Problem3
     {
         static void Main(string[] args)
         {
             // We receive from the console number of miles (parse to double)
             double miles = double.Parse(Console.ReadLine());
             
-            // We calculate miles to kilometers (double)
-            double answer = miles * 1.60934;
+            // We calculate miles to kilometers (double), changed name answer to resultMiles
+            double resultMiles = miles * 1.60934;
             
             // We print answer to the 2nd decimal place.
-            Console.WriteLine($"{answer:f2}"); 
+            Console.WriteLine($"{resultMiles:f2}"); 
         }
     }
 }
